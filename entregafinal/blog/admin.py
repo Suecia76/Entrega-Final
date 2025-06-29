71d306b69b8713e1943174a056e690fefdacd57a
Suecia76 <carrascosanti76@gmail.com>
2025-06-29 10:07:44 -0300

    hago las migraciones de usuarios

--- entregafinal/blog/admin.py
+++ entregafinal/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Libro, Avatar
+from .models import Libro
+from usuarios.models import Avatar
 
 admin.site.register(Libro)
 admin.site.register(Avatar)
